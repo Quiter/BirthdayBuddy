df0cade99a826d315ad8c582f4c10e0a6d92066d
chris <christof.quiter@gmail.com>
2026-02-17 13:54:51 +0100

    Permission hinzugefügt und Datenmodell erstellt

--- app/src/main/java/com/heckmannch/birthdaybuddy/MainActivity.kt
+++ app/src/main/java/com/heckmannch/birthdaybuddy/MainActivity.kt
@@ -45,3 +45,11 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+data class BirthdayContact(
+    val name: String,
+    val birthday: String, // Später wandeln wir das in ein echtes Datum um
+    val label: String,    // Für deine Kategorien (Privat, Arbeit, etc.)
+    val remainingDays: Int,
+    val age: Int
+)
