df6fa0c75ea44ecd8a17875cdc8ee85904256e60
chris <christof.quiter@gmail.com>
2026-02-21 20:59:52 +0100

    Removed obsolet refresh option Bugfixes and improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 3
-        versionName = "2.6.0"
+        versionName = "2.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
