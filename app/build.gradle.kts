6917dd7bc5cb2a6383c1ae5a27d6d41ee1312f2f
chris <christof.quiter@gmail.com>
2026-02-24 09:41:18 +0100

    v3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 4
-        versionName = "3.0.0"
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
