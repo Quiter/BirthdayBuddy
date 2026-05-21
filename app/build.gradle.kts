e9ca83b64c768586ded429f1d2270521c7ac6411
chris <christof.quiter@gmail.com>
2026-05-21 11:19:10 +0200

    Update markdown files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.heckmannch.birthdaybuddy"
         minSdk = 28
         targetSdk = 36
-        versionCode = 24
-        versionName = "2.3.1"
+        versionCode = 25
+        versionName = "2.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
