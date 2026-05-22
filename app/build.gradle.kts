04d22487ee2d59eb38b90acc1c5a9352f2ecac9d
Quiter <christof.quiter@gmail.com>
2026-05-22 18:42:50 +0200

    Merge remote-tracking branch 'origin/master'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 26
-        versionName = "2.3.16"
+        versionName = "2.3.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
