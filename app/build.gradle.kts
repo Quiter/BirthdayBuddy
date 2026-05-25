2d095953a5a2c636792d1b1c3da8143163ac6952
chris <christof.quiter@gmail.com>
2026-05-25 21:21:04 +0200

    Fix test error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         //noinspection ExpiredTargetSdkVersion, OldTargetApi
         targetSdk = 36
         versionCode = 29
-        versionName = "2.4.9"
+        versionName = "2.4.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
