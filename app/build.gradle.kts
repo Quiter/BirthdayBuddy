bf918ab7cbaa6bced81f536ff99aa7f946485485
chris <christof.quiter@gmail.com>
2026-06-14 14:13:39 +0200

    New version with latest improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         minSdk = 28
         targetSdk = 37
         versionCode = 34
-        versionName = "2.7.3"
+        versionName = "2.7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
