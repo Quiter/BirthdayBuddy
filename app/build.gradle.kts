9946fc1c18cbd3a34205a5bb133c1c7938d88e89
chris <christof.quiter@gmail.com>
2026-06-20 10:44:46 +0200

    Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         minSdk = 28
         targetSdk = 37
         versionCode = 37
-        versionName = "2.9.19"
+        versionName = "2.9.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
