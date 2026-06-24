1927c085cae1729bcc6d3ee2c5bfc0f564338221
chris <christof.quiter@gmail.com>
2026-06-24 15:17:43 +0200

    Fix tablet layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         minSdk = 28
         targetSdk = 37
         versionCode = 40
-        versionName = "2.11.2"
+        versionName = "2.11.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
