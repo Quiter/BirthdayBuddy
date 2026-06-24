dfa19de0a84b3e1ac76caa990209d94b4162e40b
chris <christof.quiter@gmail.com>
2026-06-24 14:55:24 +0200

    Small fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         minSdk = 28
         targetSdk = 37
         versionCode = 39
-        versionName = "2.10.18"
+        versionName = "2.11.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
