e555311989e6c4bdf5d2c3f6635ccbdb2c834ddb
chris <christof.quiter@gmail.com>
2026-07-08 13:27:29 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         minSdk = 28
         targetSdk = 37
         versionCode = 42
-        versionName = "2.11.20"
+        versionName = "2.11.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
