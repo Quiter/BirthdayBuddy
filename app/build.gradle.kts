e52ef256347fcc135084b286b5ee39242daa0fcd
chris <christof.quiter@gmail.com>
2026-07-09 15:17:56 +0200

    LazyColumn/LazyRow Items without Keys

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ configure<com.android.build.api.dsl.ApplicationExtension> {
         minSdk = 28
         targetSdk = 37
         versionCode = 42
-        versionName = "2.11.29"
+        versionName = "2.11.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
