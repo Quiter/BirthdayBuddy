099f7ee994cbe2a8501a962d4c75d843cae155a4
chris <christof.quiter@gmail.com>
2026-07-15 09:22:15 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 43
-        versionName = "2.12.8"
+        versionName = "2.12.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
