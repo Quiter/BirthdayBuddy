954eb9245b933ba3f2b64cf2544a9c67bcfc4a57
chris <christof.quiter@gmail.com>
2026-07-16 19:44:57 +0200

    Optimized settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 43
-        versionName = "2.12.16"
+        versionName = "2.12.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
