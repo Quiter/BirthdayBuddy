18f4741c4a3957922ab4ff59bb63644223edf903
chris <christof.quiter@gmail.com>
2026-07-18 00:39:43 +0200

    Nex @LabelUiExtensions.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 44
-        versionName = "2.12.18"
+        versionName = "2.12.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
